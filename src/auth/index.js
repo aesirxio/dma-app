20ab242edbc8faf32a405b65a5b923950ba26180
Tito Alvarez <augustoalvarez@gmail.com>
2022-06-19 17:35:10 -0600

    Toast alert when .env properties are missing

--- src/auth/index.js
+++ src/auth/index.js
@@ -5,7 +5,26 @@
 
 import history from '../routes/history';
 import { notify } from '../components/Toast';
-import { AesirxAuthenticationApiService, AUTHORIZATION_KEY, Storage } from 'aesirx-dma-lib';
+import {
+  AesirxAuthenticationApiService,
+  AUTHORIZATION_KEY,
+  Storage,
+  AXIOS_CONFIGS,
+  GENERAL_CONFIG,
+} from 'aesirx-dma-lib';
+
+if (
+  AXIOS_CONFIGS.CLIENT_ID === '' ||
+  AXIOS_CONFIGS.CLIENT_SECRET === '' ||
+  AXIOS_CONFIGS.BASE_ENDPOINT_URL === '' ||
+  AXIOS_CONFIGS.LICENSE === '' ||
+  GENERAL_CONFIG.WEBSOCKET_ENDPOINT === ''
+) {
+  notify(
+    'The app has not been fully configured and you will not be able to login.  Go to https://dma.aesirx.io/install-guide/npm for instructions on how to configure your .env file',
+    'error'
+  );
+}
 
 // LOGIN
 const login = async ({ username, password }) => {
