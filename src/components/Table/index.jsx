13a131307a24758e9c871dcbb8dbbd940ea61e76
giaphu <phu.tran@redweb.dk>
2022-08-19 16:13:22 +0700

    fixed #51 style event of calendar & fix change thumb view

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -443,7 +443,7 @@ const Table = ({
             />
           ) : (
             <div className="pagination d-flex align-items-center justify-content-between">
-              {pagination && page.length > 10 &&  (
+              {pagination && pagination.totalPages > 1 &&  (
                 <>
                   <PaginationComponent
                     pagination={pagination}
@@ -507,7 +507,7 @@ const Table = ({
           ) : (
             
             <div className="pagination d-flex align-items-center justify-content-between">
-              {pagination ?? (
+              {pagination && pagination.totalPages > 1 && (
                 <>
                   <PaginationComponent
                     pagination={pagination}
