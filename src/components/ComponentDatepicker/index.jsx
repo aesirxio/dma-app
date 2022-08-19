ba3351228ac92851adccf53bd8e99fbc1cf281da
giaphu <phu.tran@redweb.dk>
2022-08-19 11:41:52 +0700

    fixed #53 datepicker close every click

--- src/components/ComponentDatepicker/index.jsx
+++ src/components/ComponentDatepicker/index.jsx
@@ -28,6 +28,7 @@ class ComponentDatepicker extends React.Component {
     };
 
     this.wrapperRef = React.createRef();
+    this.pickerRef = React.createRef();
     this.handleShowPicker = this.handleShowPicker.bind(this);
     this.handleClickOutside = this.handleClickOutside.bind(this);
   }
@@ -48,11 +49,14 @@ class ComponentDatepicker extends React.Component {
     }
   }
 
-  handleShowPicker = () => {
-      this.setState((prevState) => ({
-        isOpen: !prevState.isOpen,
-      })
-    );
+  handleShowPicker = (event) => {
+    if (
+      this.state.isOpen &&
+      this.pickerRef.current &&
+      !this.pickerRef.current.contains(event.target)
+    ) {
+      this.setState({ isOpen: false });
+    } else this.setState({ isOpen: true });
   };
 
   onChange = (dates) => {
@@ -108,7 +112,7 @@ class ComponentDatepicker extends React.Component {
     let { startDate, endDate } = this.state;
 
     return (
-      <div className="p-3 bg-white rounded-3 shadow">
+      <div ref={this.pickerRef} className="p-3 bg-white rounded-3 shadow">
         <div className={className}>
           <div className="position-relative border-0">{children}</div>
         </div>
