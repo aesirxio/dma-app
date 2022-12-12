04ecb2944d517b82f7202a4bcd48e8dc869368ae
huy <kakahuy113@gmail.com>
2022-12-12 16:19:30 +0700

    #164: DMA intergration

---
+++ src/integration/AesirXDmaChannels/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Toast } from 'components/Toast';
+import ErrorBoundary from 'layouts/ErrorBoundary';
+import i18n from 'translations/i18n';
+import { I18nextProvider } from 'react-i18next';
+import ChannelsPage from 'containers/ChannelsPage';
+
+const AesirXDmaChannels = (props) => {
+  return (
+    <ErrorBoundary>
+      <I18nextProvider i18n={i18n}>
+        <Toast />
+        <ChannelsPage {...props} />
+      </I18nextProvider>
+    </ErrorBoundary>
+  );
+};
+
+export default AesirXDmaChannels;
