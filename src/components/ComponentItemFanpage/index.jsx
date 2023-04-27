8e98cedf7c67456ea637d838811edd9637d8a436
Lu Nguyen <lu@r-digital.tech>
2023-04-27 13:52:06 +0700

    Format

--- src/components/ComponentItemFanpage/index.jsx
+++ src/components/ComponentItemFanpage/index.jsx
@@ -5,7 +5,7 @@
 
 import React from 'react';
 import { Checkbox } from 'aesirx-uikit';
-import {Image as ComponentImage} from "aesirx-uikit";
+import { Image as ComponentImage } from 'aesirx-uikit';
 
 class ComponentItemFanpage extends React.Component {
   constructor(props) {
