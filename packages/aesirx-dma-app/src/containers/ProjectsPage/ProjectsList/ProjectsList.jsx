ce4ab863a056bf1fff7e16b109552f12654111d5
lamanhvu62 <103493256+lamanhvu62@users.noreply.github.com>
2023-09-29 15:16:31 +0700

    Update ProjectsList.jsx

--- packages/aesirx-dma-app/src/containers/ProjectsPage/ProjectsList/ProjectsList.jsx
+++ packages/aesirx-dma-app/src/containers/ProjectsPage/ProjectsList/ProjectsList.jsx
@@ -12,7 +12,7 @@ import { observer } from 'mobx-react';
 import { withProjectViewModel } from '../ProjectViewModels/ProjectViewModelContextProvider';
 import { PROJECT_COLUMN_INDICATOR } from '../../../constants/ProjectModule';
 
-import { Spinner, Table, TableBar, Thumb } from 'aesirx-uikit';
+import { Spinner, Table, TableBar } from 'aesirx-uikit';
 import ComponentNoData from '../../../components/ComponentNoData';
 import ComponentViewList from '../../../components/ComponentViewList';
 
@@ -170,7 +170,7 @@ const ProjectsList = observer(
                 pagination={pagination}
               />
             ) : (
-              <Thumb data={projects} />
+              ''
             )
           ) : (
             <ComponentNoData
